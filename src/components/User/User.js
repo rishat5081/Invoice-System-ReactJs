6e12e05af81807c069cbaa8922bcbf1e1a255c8a
Engineer S. Saad <rishat.5081@gmail.com>
2022-04-09 00:37:28 +0500

    Updating the User

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -1,7 +1,7 @@
 import { useEffect, useState, Fragment } from "react";
 import { useFormWithYup } from "hooks";
 import { details, table } from "constants/components/user";
-import { OverviewTable, Icon, SubmitButton, Input } from "components";
+import { OverviewTable, Icon, SubmitButton, Input, Spinner } from "components";
 import * as S from "./styles";
 import { GetUserByIdAPI } from "../../Axios APIs/Admin APIs/Adminapis";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -18,20 +18,20 @@ import {
 const User = ({ userId }) => {
   const { register, errors, control } = useFormWithYup(schema);
   const [fields, setFields] = useState([]);
+  const [isWaiting, setIsWaiting] = useState(true);
   useEffect(async () => {
     // This will be use to fetch
     // supplier information from endpoints
     // with {supplierId}
-    console.log("userId", userId);
+
     const details = await GetUserByIdAPI(userId);
-    console.log("details", details);
     const arr = [];
     arr.push({
       id: details.id,
       name: details.firstName,
       value: details.firstName,
       type: "text",
-      placeholder: "e.g John Smith",
+      placeholder: "e.g John",
       label: "First Name",
     });
     arr.push({
@@ -39,7 +39,7 @@ const User = ({ userId }) => {
       name: details.lastName,
       value: details.lastName,
       type: "text",
-      placeholder: "e.g John Smith",
+      placeholder: "e.g Smith",
       label: "Last Name",
     });
     arr.push({
@@ -47,10 +47,11 @@ const User = ({ userId }) => {
       name: details.email,
       value: details.email,
       type: "text",
-      placeholder: "e.g John Smith",
+      placeholder: "e.g smith@gmail.com",
       label: "Email",
     });
     setFields(arr);
+    setIsWaiting(false);
   }, []);
 
   const handleSubmit = (data) => {
@@ -59,39 +60,47 @@ const User = ({ userId }) => {
 
   return (
     <S.Supplier>
-      <div class="d-flex justify-content-end">
-        <div className="p-2 text-danger">
-          <FontAwesomeIcon icon={faTrash} />
-        </div>
-      </div>
-      <div className="p-2 mb-5 ">
-        <form onSubmit={() => handleSubmit()}>
-          {fields.length < 0 ? (
-            <Spinner />
-          ) : (
-            fields.map(({ id, name, label, value, type, ...rest }, index) => (
-              <Fragment key={index}>
-                <Input
-                  style={{
-                    width: "50%",
-                    color: "#2C7BE5",
-                    fontWeight: 500,
-                  }}
-                  key={id}
-                  value={value === null ? "" : value}
-                  label={label}
-                  error={errors[name]?.message}
-                  type={type}
-                  {...register(name)}
-                  {...rest}
-                />
-              </Fragment>
-            ))
-          )}
+      {isWaiting === true ? (
+        <Spinner />
+      ) : (
+        <>
+          <div class="d-flex justify-content-end">
+            <div className="p-2 text-danger">
+              <FontAwesomeIcon icon={faTrash} />
+            </div>
+          </div>
+          <div className="p-2 mb-5 ">
+            <form onSubmit={() => handleSubmit()}>
+              {fields.length < 0 ? (
+                <Spinner />
+              ) : (
+                fields.map(
+                  ({ id, name, label, value, type, ...rest }, index) => (
+                    <Fragment key={index}>
+                      <Input
+                        style={{
+                          width: "50%",
+                          color: "#2C7BE5",
+                          fontWeight: 500,
+                        }}
+                        key={id}
+                        value={value === null ? "" : value}
+                        label={label}
+                        error={errors[name]?.message}
+                        type={type}
+                        {...register(name)}
+                        {...rest}
+                      />
+                    </Fragment>
+                  )
+                )
+              )}
 
-          <SubmitButton>Update</SubmitButton>
-        </form>
-      </div>
+              <SubmitButton>Update</SubmitButton>
+            </form>
+          </div>
+        </>
+      )}
     </S.Supplier>
   );
 };
