e33655c575ba974cf483b1df463faac387762394
Engineer S. Saad <rishat.5081@gmail.com>
2022-05-17 20:12:24 +0500

    Add the API constant of Get all invoices

--- src/Axios APIs/Axios/filesConstants.js
+++ src/Axios APIs/Axios/filesConstants.js
@@ -1,2 +1,3 @@
 export const FilesUpload = "/api/UserFile/AddFiles";
 export const AddFilesDetails = "/api/UserFile/AddFilesData";
+export const GetAllInvoiceFiles = "/api/User/GetAllUserInvoiceFile";
